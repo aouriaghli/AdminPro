d911837f301b486cc2a0424ab66f47ca726f96d7
aouriaghli <ayb.job@gmail.com>
2023-09-29 12:50:31 +0200

    Sec19. Aplicacion funcional v1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api'
+  baseUrl: 'https://04-backend-server-production.up.railway.app/api'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,6 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:3000/api'
+  // production: true,
+  // baseUrl: 'https://04-backend-server-production.up.railway.app/api'
 }
